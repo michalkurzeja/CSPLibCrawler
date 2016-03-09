b3f00df1f6ccf02da3cef4b4e6916700a2d3dec1
khelle <jamroz.kamil@gmail.com>
2016-03-09 19:46:51 +0100

    Added params.json and paramterization of app

--- src/main.js
+++ src/main.js
@@ -1,20 +1,24 @@
 GLOBAL.app = require('auto-loader').load(__dirname);
 
-app.params = {
-    user: 'Kamil',
-    password: 'Kamil1',
-    lokiPath: 'http://localhost/dokuwiki/doku.php?id='
-};
-
 require("bluebird").promisifyAll(require("request"));
 
 module.exports.crawl = function() {
+
+    app.params = JSON.parse(require('fs').readFileSync('../data/params.json', 'utf8'));
+
+    process.argv.forEach(function (val, index, array) {
+        if (val.indexOf('=') > -1) {
+            var keyVal = val.replace(/-/g, '').split('=');
+            app.params[keyVal[0]] = keyVal[1];
+        }
+    });
+
     var authenticator = new app.Auth.Authenticator;
 
     authenticator
         .login()
         .then(function(data) {
-            console.log(data);
+            console.log('Success');
         })
     ;
 };
